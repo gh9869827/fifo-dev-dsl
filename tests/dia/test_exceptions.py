0907f4f3b57a981ac7f64d36c96e0071d737b4f7
Flo <156147465+gh9869827@users.noreply.github.com>
2025-06-08 19:26:20 -0600

    Add tests for runtime exceptions

---
+++ tests/dia/test_exceptions.py
@@ -0,0 +1,16 @@
+import pytest
+from fifo_dev_dsl.dia.runtime.exceptions import ApiErrorRetry, ApiErrorAbortAndResolve
+
+
+def test_api_error_retry_construction():
+    message = "retry"
+    exc = ApiErrorRetry(message)
+    assert isinstance(exc, ApiErrorRetry)
+    assert str(exc) == message
+
+
+def test_api_error_abort_and_resolve_construction():
+    message = "abort"
+    exc = ApiErrorAbortAndResolve(message)
+    assert isinstance(exc, ApiErrorAbortAndResolve)
+    assert str(exc) == message
